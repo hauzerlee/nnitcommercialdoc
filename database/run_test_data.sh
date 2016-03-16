93a466693c968173a8e416ad6abd594d66b750d3
jaly <sjl.jaly@gmail.com>
2016-03-16 22:18:45 +0800

    update testing data

--- database/run_test_data.sh
+++ database/run_test_data.sh
@@ -2,4 +2,4 @@
 host=127.0.0.1
 port=6379
 password=
-cat test_data_run.json | /Users/solution/redis303/src/redis-cli -h $host -p $port -a $password --pipe
+cat test_data_run.json | redis-cli -h $host -p $port -a $password --pipe
